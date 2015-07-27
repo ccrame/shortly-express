36c353c788c6df7525c634384d9dcf872ae81eb8
Charles Crame <ccrame@gmail.com>
2015-07-27 15:38:56 -0700

    defined relationship between link and user

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,14 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var Link = require('./link');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  
+  urls: function(){
+    return this.hasMany(Link);
+  }
 });
 
 module.exports = User;
